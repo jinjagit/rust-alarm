a6928351554a8202ecd0c3a4c7c3e41c0e3f90bb
Simon Tharby <simontharby@gmail.com>
2022-10-09 18:54:33 +0100

    handle time offset giving time less than 0 hours

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
 mod display_data;
 use display_data::display_data;
 use chrono::{Timelike, Utc};
-use std::{io::{stdout}, thread, time};
+use std::{io::stdout, thread, time};
 use crossterm::{ExecutableCommand, cursor};
 
 fn main() {
     let mut stdout = stdout();
     let display_data = display_data();
-    let offset: u32 = 1;
+    let offset: i32 = 1;
     let one_second = time::Duration::from_millis(1000);
 
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Clear terminal
@@ -15,13 +15,16 @@ fn main() {
 
     loop {
         let now = Utc::now();
-        let mut hour = now.hour() + offset;
+        let mut hour:i32 = now.hour() as i32 + offset;
     
         if hour > 24 {
-            hour = 0;
+            hour = hour - 24;
+        }
+
+        if hour < 0 {
+            hour = 24 - hour;
         }
     
-        // print!("\x1B[2J\x1B[1;0H");
         stdout.execute(cursor::MoveTo(0,0)).ok();
 
         println!(
