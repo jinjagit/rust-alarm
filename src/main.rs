202dfe933168a69da7d40705c1f0f56e526e6209
Simon Tharby <simontharby@gmail.com>
2022-10-09 17:18:48 +0100

    print time (with hour offset) every second

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,25 @@
-fn main() {
-    let a: i32 = 5;
-    let b: i32 = 37;
+use chrono::{Timelike, Utc};
+use std::{thread, time};
 
-    println!("The answer is {:?}", a + b);
+fn main() {
+    let offset: u32 = 1;
+    let one_second = time::Duration::from_millis(1000);
+
+    loop {
+        let now = Utc::now();
+        let mut hour = now.hour() + offset;
+    
+        if hour > 24 {
+            hour = 0;
+        }
+    
+        println!(
+            "The current time is {:02}:{:02}:{:02}",
+            hour,
+            now.minute(),
+            now.second()
+        );
+
+        thread::sleep(one_second);
+    }
 }
