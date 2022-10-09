6bfbdb54f5dbd40955277e1394d9f3f34bdb2b9d
Simon Tharby <simontharby@gmail.com>
2022-10-09 19:06:13 +0100

    simplify fn main by moving some content to functions

--- src/display_data.rs
+++ src/display_data.rs
@@ -1,4 +1,4 @@
 pub fn display_data() -> Vec<u32> {
-  return [2, 3, 3].to_vec();
+  [2, 3, 3].to_vec()
 }
 
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 mod display_data;
 use display_data::display_data;
-use chrono::{Timelike, Utc};
+use chrono::{Timelike, Utc, DateTime};
 use std::{io::stdout, thread, time};
 use crossterm::{ExecutableCommand, cursor};
 
@@ -15,28 +15,36 @@ fn main() {
 
     loop {
         let now = Utc::now();
-        let mut hour:i32 = now.hour() as i32 + offset;
-    
-        if hour > 24 {
-            hour = hour - 24;
-        }
-
-        if hour < 0 {
-            hour = 24 - hour;
-        }
+        let hour:i32 = correct_hour_with_offset(now.hour() as i32 + offset);
     
         stdout.execute(cursor::MoveTo(0,0)).ok();
 
-        println!(
-            "The current time is {:02}:{:02}:{:02}",
-            hour,
-            now.minute(),
-            now.second()
-        );
-
-        println!();
-        println!("display_data: {:?}", display_data);
+        time_display(now, hour, display_data.clone());
 
         thread::sleep(one_second);
     }
+}
+
+fn correct_hour_with_offset(mut hour: i32) -> i32 {
+    if hour > 24 {
+        hour = hour - 24;
+    }
+
+    if hour < 0 {
+        hour = 24 - hour;
+    }
+
+    hour
+}
+
+fn time_display(now: DateTime<Utc>, hour: i32, display_data: Vec<u32>) {
+    println!(
+        "The current time is {:02}:{:02}:{:02}",
+        hour,
+        now.minute(),
+        now.second()
+    );
+
+    println!();
+    println!("display_data: {:?}", display_data);
 }
