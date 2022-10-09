e14de3e408224a6dd7feda06e66630136f68a99a
Simon Tharby <simontharby@gmail.com>
2022-10-09 18:29:17 +0100

    use crossterm crate for better control of terminal

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 use chrono::{Timelike, Utc};
-use std::{io::{stdout, Write},thread, time};
+use std::{io::{stdout}, thread, time};
+use crossterm::{ExecutableCommand, cursor};
 
 fn main() {
     let mut stdout = stdout();
     let offset: u32 = 1;
     let one_second = time::Duration::from_millis(1000);
 
-    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Clear terminal
+    stdout.execute(cursor::Hide).ok();
 
     loop {
         let now = Utc::now();
@@ -17,14 +19,18 @@ fn main() {
         }
     
         // print!("\x1B[2J\x1B[1;0H");
-        print!(
-            "\rThe current time is {:02}:{:02}:{:02}",
+        stdout.execute(cursor::MoveTo(0,0)).ok();
+
+        println!(
+            "The current time is {:02}:{:02}:{:02}",
             hour,
             now.minute(),
             now.second()
         );
 
-        stdout.flush().unwrap();
+        println!();
+        println!("something else");
+
         thread::sleep(one_second);
     }
 }
