a9fcbe82706af00a311a8f415fd369ca325631e1
Simon Tharby <simontharby@gmail.com>
2022-10-09 17:34:56 +0100

    Clear terminal & posn cursor top left at start, then overwrite line on output

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
 use chrono::{Timelike, Utc};
-use std::{thread, time};
+use std::{io::{stdout, Write},thread, time};
 
 fn main() {
+    let mut stdout = stdout();
     let offset: u32 = 1;
     let one_second = time::Duration::from_millis(1000);
+
+    print!("{esc}[2J{esc}[1;1H", esc = 27 as char);
 
     loop {
         let now = Utc::now();
@@ -13,13 +16,15 @@ fn main() {
             hour = 0;
         }
     
-        println!(
-            "The current time is {:02}:{:02}:{:02}",
+        // print!("\x1B[2J\x1B[1;0H");
+        print!(
+            "\rThe current time is {:02}:{:02}:{:02}",
             hour,
             now.minute(),
             now.second()
         );
 
+        stdout.flush().unwrap();
         thread::sleep(one_second);
     }
 }
