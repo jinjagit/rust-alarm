b2e25cd8ffe18b2354133b4a47280f8ce75e526b
Simon Tharby <simontharby@gmail.com>
2022-10-10 21:54:43 +0100

    time display flashes when alarm active

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,14 @@ fn main() {
     let mut stdout = stdout();
     let digits_data = digits_data();
     let offset: i32 = 1;
-    let one_second = time::Duration::from_millis(1000);
+    let half_a_second = time::Duration::from_millis(500);
+    let mut tick = 0;
+    let mut alert_on = false;
+
+    let alarm_hour = 21;
+    let alarm_min = 50;
+
+    let alarm_time = format!("{}:{}", alarm_hour, alarm_min);
 
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Clear terminal
     stdout.execute(cursor::Hide).ok();
@@ -16,10 +23,22 @@ fn main() {
     loop {
         let now = Utc::now();
         let hour:u32 = correct_hour_with_offset(now.hour(), offset);
+        let minute = now.minute();
+        let second = now.second();
 
-        time_display(now, hour, digits_data.clone());
+        if tick == 2 {
+            tick = 0;
+        }
 
-        thread::sleep(one_second);
+        if alarm_hour == hour && alarm_min == minute {
+            alert_on = true;
+        }
+
+        time_display(hour, minute, second, &alarm_time, digits_data.clone(), tick, alert_on);
+
+        tick += 1;
+
+        thread::sleep(half_a_second);
     }
 }
 
@@ -37,43 +56,51 @@ fn correct_hour_with_offset(hour: u32, offset: i32) -> u32 {
     corrected as u32
 }
 
-fn time_display(now: DateTime<Utc>, hour: u32, digits_data: Vec<Vec<String>>) {
-    let minute = now.minute();
+fn time_display(hour: u32, minute: u32, second: u32, alarm_time: &String, digits_data: Vec<Vec<String>>, tick: i32, alert_on: bool) {
     let digit_1 = (hour - (hour % 10)) / 10;
     let digit_2 = hour % 10;
     let digit_3 = (minute - (minute % 10)) / 10;
     let digit_4 = minute % 10;
     let mut stdout = stdout();
 
-    stdout.execute(cursor::MoveTo(0,0)).ok();
-    print!("{}", digits_data[digit_1 as usize][0]);
-    print!("{}", digits_data[digit_2 as usize][0]);
-    print!("  ");
-    print!("{}", digits_data[digit_3 as usize][0]);
-    print!("{}", digits_data[digit_4 as usize][0]);
-    println!("        ");
+    if alert_on == false || tick == 0 {
+        stdout.execute(cursor::MoveTo(0,0)).ok();
+        print!("{}", digits_data[digit_1 as usize][0]);
+        print!("{}", digits_data[digit_2 as usize][0]);
+        print!("  ");
+        print!("{}", digits_data[digit_3 as usize][0]);
+        print!("{}", digits_data[digit_4 as usize][0]);
+        println!("        ");
 
-    stdout.execute(cursor::MoveTo(0,1)).ok();
-    print!("{}", digits_data[digit_1 as usize][1]);
-    print!("{}", digits_data[digit_2 as usize][1]);
-    print!(" \u{2580}");
-    print!("{}", digits_data[digit_3 as usize][1]);
-    print!("{}", digits_data[digit_4 as usize][1]);
-    println!("        ");
+        stdout.execute(cursor::MoveTo(0,1)).ok();
+        print!("{}", digits_data[digit_1 as usize][1]);
+        print!("{}", digits_data[digit_2 as usize][1]);
+        print!(" \u{2580}");
+        print!("{}", digits_data[digit_3 as usize][1]);
+        print!("{}", digits_data[digit_4 as usize][1]);
+        println!("        ");
 
-    stdout.execute(cursor::MoveTo(0,2)).ok();
-    print!("{}", digits_data[digit_1 as usize][2]);
-    print!("{}", digits_data[digit_2 as usize][2]);
-    print!(" \u{2580}");
-    print!("{}", digits_data[digit_3 as usize][2]);
-    print!("{}", digits_data[digit_4 as usize][2]);
-    print!(" :{:02}", now.second());
-    println!("        ");
+        stdout.execute(cursor::MoveTo(0,2)).ok();
+        print!("{}", digits_data[digit_1 as usize][2]);
+        print!("{}", digits_data[digit_2 as usize][2]);
+        print!(" \u{2580}");
+        print!("{}", digits_data[digit_3 as usize][2]);
+        print!("{}", digits_data[digit_4 as usize][2]);
+        if tick == 0 {
+            print!(" :{:02}", second);
+        } else {
+            print!("  {:02}", second);
+        }
+        println!("        ");
+    } else {
+        print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Clear terminal
+    }
 
     stdout.execute(cursor::MoveTo(0,3)).ok();
     println!();
 
     stdout.execute(cursor::MoveTo(0,4)).ok();
-    println!(" \u{25F7} Alarm set for --:--");
+    print!(" \u{25F7} Alarm set for ");
+    println!("{}", alarm_time);
     // println!(" snooze:spc, reset:rtn");
 }
