6d7e62f8e8898dacde5d3c39eb561e884ed7a824
Simon Tharby <simontharby@gmail.com>
2022-10-16 19:45:45 +0100

    Refactor to use new AlarmData struct

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
 mod digits_data;
 use digits_data::digits_data;
-use chrono::{Timelike, Utc, DateTime};
+use chrono::{Timelike, Utc};
 use std::{io::stdout, thread, time};
 use crossterm::{ExecutableCommand, cursor};
+
+struct AlarmData {
+    active: bool,
+    hour: u32,
+    min: u32
+}
 
 fn main() {
     let mut stdout = stdout();
@@ -10,12 +16,12 @@ fn main() {
     let offset: i32 = 1;
     let half_a_second = time::Duration::from_millis(500);
     let mut tick = 0;
-    let mut alert_on = false;
 
-    let alarm_hour = 21;
-    let alarm_min = 50;
-
-    let alarm_time = format!("{}:{}", alarm_hour, alarm_min);
+    let mut alarm = AlarmData {
+        active: false,
+        hour: 19,
+        min: 42
+    };
 
     print!("{esc}[2J{esc}[1;1H", esc = 27 as char); // Clear terminal
     stdout.execute(cursor::Hide).ok();
@@ -26,15 +32,11 @@ fn main() {
         let minute = now.minute();
         let second = now.second();
 
-        if tick == 2 {
-            tick = 0;
-        }
+        if tick == 2 { tick = 0; }
 
-        if alarm_hour == hour && alarm_min == minute {
-            alert_on = true;
-        }
+        if alarm.hour == hour && alarm.min == minute { alarm.active = true; }
 
-        time_display(hour, minute, second, &alarm_time, digits_data.clone(), tick, alert_on);
+        time_display(hour, minute, second, &alarm, digits_data.clone(), tick);
 
         tick += 1;
 
@@ -45,25 +47,20 @@ fn main() {
 fn correct_hour_with_offset(hour: u32, offset: i32) -> u32 {
     let mut corrected = hour as i32 + offset;
 
-    if corrected >= 24 {
-        corrected = corrected - 24;
-    }
-
-    if corrected < 0 {
-        corrected = 24 - corrected;
-    }
+    if corrected >= 24 { corrected = corrected - 24; }
+    if corrected < 0 { corrected = 24 - corrected; }
 
     corrected as u32
 }
 
-fn time_display(hour: u32, minute: u32, second: u32, alarm_time: &String, digits_data: Vec<Vec<String>>, tick: i32, alert_on: bool) {
+fn time_display(hour: u32, minute: u32, second: u32, alarm: &AlarmData, digits_data: Vec<Vec<String>>, tick: i32) {
     let digit_1 = (hour - (hour % 10)) / 10;
     let digit_2 = hour % 10;
     let digit_3 = (minute - (minute % 10)) / 10;
     let digit_4 = minute % 10;
     let mut stdout = stdout();
 
-    if alert_on == false || tick == 0 {
+    if alarm.active == false || tick == 0 {
         stdout.execute(cursor::MoveTo(0,0)).ok();
         print!("{}", digits_data[digit_1 as usize][0]);
         print!("{}", digits_data[digit_2 as usize][0]);
@@ -101,6 +98,6 @@ fn time_display(hour: u32, minute: u32, second: u32, alarm_time: &String, digits
 
     stdout.execute(cursor::MoveTo(0,4)).ok();
     print!(" \u{25F7} Alarm set for ");
-    println!("{}", alarm_time);
+    println!("{:02}:{:02}", alarm.hour, alarm.min);
     // println!(" snooze:spc, reset:rtn");
 }
